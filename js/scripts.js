74f89981260b760942f7d35bfdb02195cd6286c3
Cassie Musolf and Katy Daviscourt <cassie@>
2017-01-25 14:02:11 -0800

    add javascript code that makes roll and hold buttons function properly for both players. Players current round rolls and sum will show with each roll, and when hold is pressed that sum will be added to their total score at top

--- js/scripts.js
+++ js/scripts.js
@@ -4,55 +4,56 @@ function rollDie() {
   return Math.floor( Math.random() * ( 1 + 6 - 1 ) ) + 1;
 }
 
-var resultsArray1 = [];
-var resultsArray2 = [];
-var totalArray1 = [];
-var totalArray2 = [];
+function Player() {
+  this.resultsArray = [];
+  this.total = 0;
+}
 
-
+Player.prototype.calcSum = function() {
+  var arraySum = 0;
+  for(var i=0; i<this.resultsArray.length; i++){
+    arraySum += parseInt(this.resultsArray[i]);
+  };
+  return arraySum;
+}
 
 //front-end logic
 
 $(document).ready(function() {
+  var playerOne = new Player();
+  var playerTwo = new Player();
+
   $("button#roll1").click(function() {
-
     var thisRoll = rollDie();
-    resultsArray1.push(" " + thisRoll);
-
-    var arraySum1 = 0;
-    for(var i=0; i<resultsArray1.length; i++){
-    arraySum1 += parseInt(resultsArray1[i]);
-  };
-
-  $("span.currentScore1").text(thisRoll);
-  $("span.roundScore1").text(resultsArray1 + " " + "=" + " " + arraySum1);
-  });
-
-
-  $("button#roll2").click(function() {
-
-    var thisRoll = rollDie();
-    resultsArray2.push(" " + thisRoll);
-
-    var arraySum2 = 0;
-    for(var i=0; i<resultsArray2.length; i++){
-    arraySum2 += parseInt(resultsArray2[i]);
-  };
-
-    $("span.currentScore2").text(thisRoll);
-    $("span.roundScore2").text(resultsArray2 + " " + "=" + " " + arraySum2);
+    playerOne.resultsArray.push(" " + thisRoll);
+    $("span.currentScore1").text(thisRoll);
+    $("span.roundScore1").text(playerOne.resultsArray + " " + "=" + " " + playerOne.calcSum());
   });
 
   $("button#hold1").click(function() {
 
-    totalArray1.push(arraySum1);
+    playerOne.total += playerOne.calcSum();
+    playerOne.resultsArray = [];
+    $("span.currentScore1").text("");
+    $("span.roundScore1").text("");
+    $("span.playerOneTotal").text(playerOne.total);
 
-    var totalSum1 = 0;
-    for(var i=0; i<totalArray1.length; i++){
-    totalSum1 += parseInt(totalArray1[i]);
-  };
+  });
 
-  $("span.playerOneTotal").text(totalSum1);
+  $("button#roll2").click(function() {
+    var thisRoll = rollDie();
+    playerTwo.resultsArray.push(" " + thisRoll);
+    $("span.currentScore2").text(thisRoll);
+    $("span.roundScore2").text(playerTwo.resultsArray + " " + "=" + " " + playerTwo.calcSum());
+  });
+
+  $("button#hold2").click(function() {
+
+    playerTwo.total += playerTwo.calcSum();
+    playerTwo.resultsArray = [];
+    $("span.currentScore2").text("");
+    $("span.roundScore2").text("");
+    $("span.playerTwoTotal").text(playerTwo.total);
 
   });
 
